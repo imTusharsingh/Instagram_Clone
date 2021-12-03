008a465520b377f01baa1e4b7a0a600235a57814
Tushar Singh <rajputtushar309@gmail.com>
2021-12-03 16:21:11 +0530

    changed the paths

--- app.js
+++ app.js
@@ -28,14 +28,10 @@ app.use(require('./routes/follow'));
 if ( process.env.NODE_ENV == "production"){
 
     app.use(express.static("client/build"));
-
-    // const path = require("path");
-
-    // app.get("*", (req, res) => {
-
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    // })
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 
 
 }
